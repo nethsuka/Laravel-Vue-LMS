ee5a06db4992e4815e833b7ff21fefb444174b0e
Sanuja Nethsuka <nethsuka888@gmail.com>
2024-08-13 19:45:17 +0530

    add files

---
+++ resources/js/Pages/Auth/Register.vue
@@ -0,0 +1,233 @@
+<script setup>
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+import GuestLayoutRegister from '@/Layouts/GuestLayoutRegister.vue';
+
+const form = useForm({
+    name: '',
+    NIC: '',
+    exam_year: '',
+    school: '',
+    getToKnow: '',
+    address : '',
+    f_number: '',
+    m_number: '',
+    whatsapp_number: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+});
+
+const submit = () => {
+    form.post(route('register'), {
+        onFinish: () => form.reset('password', 'password_confirmation'),
+    });
+};
+</script>
+
+<template>
+    <GuestLayoutRegister>
+        <Head title="Register" />
+
+        <form @submit.prevent="submit">
+            <div>
+                <InputLabel for="name" value="Name*" />
+
+                <TextInput
+                    id="name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.name"
+                    required
+                    autofocus
+                    autocomplete="name"
+                    placeholder="First Name & Last Name"
+                />
+
+                <InputError class="mt-2" :message="form.errors.name" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="nic" value="Student NIC Number*" />
+
+                <TextInput
+                    id="nic"
+                    type="number"
+                    class="mt-1 block w-full"
+                    v-model="form.NIC"
+                    required
+                    autofocus
+                    autocomplete="nic"
+                />
+
+                <InputError class="mt-2" :message="form.errors.NIC" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="year" value="Exam Year*" />
+
+                <TextInput
+                    id="year"
+                    type="number"
+                    class="mt-1 block w-full"
+                    v-model="form.exam_year"
+                    required
+                    autofocus
+                    autocomplete="year"
+                />
+
+                <InputError class="mt-2" :message="form.errors.exam_year" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="school" value="School" />
+
+                <TextInput
+                    id="school"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.school"
+                    autofocus
+                    autocomplete="school"
+                />
+
+                <InputError class="mt-2" :message="form.errors.school" />
+            </div>
+
+            <div class="mt-4">
+                <label for="know" class="block font-medium text-sm text-gray-700">How did you get to know about the teacher ?*</label>
+                <textarea 
+                    id="know" 
+                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" 
+                    name="w3review" rows="3"
+                    v-model="form.getToKnow"
+                    required
+                    autofocus
+                ></textarea>
+
+                <InputError class="mt-2" :message="form.errors.getToKnow" />
+            </div>
+
+            <div class="mt-4">
+                <label for="address" class="block font-medium text-sm text-gray-700">Address (With Town)*</label>
+                <textarea 
+                    id="address" 
+                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" 
+                    rows="2"
+                    v-model="form.address"
+                    required
+                    autofocus
+                ></textarea>
+
+                <InputError class="mt-2" :message="form.errors.address" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="fnum" value="Father's Number" />
+
+                <TextInput
+                    id="fnum"
+                    type="tel"
+                    class="mt-1 block w-full"
+                    v-model="form.f_number"
+                    autofocus
+                    autocomplete="Father's Number"
+                />
+
+                <InputError class="mt-2" :message="form.errors.f_number" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="mnum" value="Mother's Number" />
+
+                <TextInput
+                    id="mnum"
+                    type="tel"
+                    class="mt-1 block w-full"
+                    v-model="form.m_number"
+                    autofocus
+                    autocomplete="Mother's Number"
+                />
+
+                <InputError class="mt-2" :message="form.errors.m_number" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="wnum" value="Whatsapp Number*" />
+
+                <TextInput
+                    id="wnum"
+                    type="tel"
+                    class="mt-1 block w-full"
+                    v-model="form.whatsapp_number"
+                    autofocus
+                    required
+                    autocomplete="whatsapp_number"
+                />
+
+                <InputError class="mt-2" :message="form.errors.whatsapp_number" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="email" value="Email*" />
+
+                <TextInput
+                    id="email"
+                    type="email"
+                    class="mt-1 block w-full"
+                    v-model="form.email"
+                    required
+                    autocomplete="username"
+                />
+
+                <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password" value="Password" />
+
+                <TextInput
+                    id="password"
+                    type="password"
+                    class="mt-1 block w-full"
+                    v-model="form.password"
+                    required
+                    autocomplete="new-password"
+                />
+
+                <InputError class="mt-2" :message="form.errors.password" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password_confirmation" value="Confirm Password" />
+
+                <TextInput
+                    id="password_confirmation"
+                    type="password"
+                    class="mt-1 block w-full"
+                    v-model="form.password_confirmation"
+                    required
+                    autocomplete="new-password"
+                />
+
+                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <Link
+                    :href="route('login')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Already registered?
+                </Link>
+
+                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Register
+                </PrimaryButton>
+            </div>
+        </form>
+    </GuestLayoutRegister>
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,130 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+const activeTab = ref('profile');
+
+function setActiveTab(tab) {
+  activeTab.value = tab;
+}
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg px-6 py-3">
+                    <!-- <div class="p-6 text-gray-900">You're logged in!</div> -->
+                    <!-- <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+                    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file">
+                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p> -->
+
+                    <div class="md:flex">
+                        <ul class="flex-column space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
+                        <li>
+                            <a
+                            href="#"
+                            class="inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+                            :class="{ 'back-color': activeTab === 'profile', 'dark:bg-blue-600': activeTab === 'profile' }"
+                            @click.prevent="setActiveTab('profile')"
+                            aria-current="page"
+                            >
+                            2023&nbsp;A/L
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                            href="#"
+                            class="inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+                            :class="{ 'back-color': activeTab === 'dashboard', 'dark:bg-gray-800': activeTab === 'dashboard' }"
+                            @click.prevent="setActiveTab('dashboard')"
+                            >
+                            2024&nbsp;A/L
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                            href="#"
+                            class="inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+                            :class="{ 'back-color': activeTab === 'settings', 'dark:bg-gray-800': activeTab === 'settings' }"
+                            @click.prevent="setActiveTab('settings')"
+                            >
+                            2025&nbsp;A/L
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                            href="#"
+                            class="inline-flex items-center px-4 py-3 text-gray-400 rounded-lg cursor-not-allowed bg-gray-50 w-full dark:bg-gray-800 dark:text-gray-500"
+                            :class="{ 'back-color': activeTab === 'disabled', 'dark:bg-gray-800': activeTab === 'disabled' }"
+                            >
+                            Disabled
+                            </a>
+                        </li>
+                        </ul>
+
+                        <div class="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full">
+                        <template v-if="activeTab === 'profile'">
+
+                            <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">2023 A/L Class</h3>
+                            <!-- <p class="mb-2">This is some placeholder content for the Profile tab's associated content. Clicking another tab will toggle the visibility of this one.</p>
+                            <p>The tab JavaScript swaps classes to control the content visibility and styling.</p> -->
+                            <div class="mt-5 mb-10">
+                            <a href="#" class="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                                Join Online Class
+                            </a>
+                            <a href="#" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                                Telegram Group
+                            </a>
+                        </div>
+
+                        <div class="pb-9">
+                            <h6 class="pb-1"><b>Papers</b></h6>
+                            <hr class="pb-3">
+                            <a href="#" class="block max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <!-- <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5> -->
+                            <p class="font-normal text-gray-700 dark:text-gray-400">My_file.pdf</p>
+                            </a>
+                        </div>
+
+
+                        <h6 class="pb-1"><b>Recordings</b></h6>
+                        <hr class="pb-3">
+                        <iframe src="https://player.vimeo.com/video/910151320?h=2dbfbb67e8" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+
+                        </template>
+                        <template v-if="activeTab === 'dashboard'">
+                            <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Dashboard Tab</h3>
+                            <p class="mb-2">This is some placeholder content for the Dashboard tab's associated content.</p>
+                        </template>
+                        <template v-if="activeTab === 'settings'">
+                            <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Settings Tab</h3>
+                            <p class="mb-2">This is some placeholder content for the Settings tab's associated content.</p>
+                        </template>
+                        <template v-if="activeTab === 'contact'">
+                            <h3 class="text-lg font-bold text-gray-900 dark:text-white mb-2">Contact Tab</h3>
+                            <p class="mb-2">This is some placeholder content for the Contact tab's associated content.</p>
+                        </template>
+                        </div>
+                    </div>
+                                        
+
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<style scoped>
+.back-color { 
+    background-color: #2f7dd1; 
+    color: white;
+}
+</style>
