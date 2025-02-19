23ae698c3485c168d0927c951252abb49ade683e
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-02-19 22:18:11 +0530

    Configure Vite server to use specific host and enable HMR for improved development experience

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,11 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'lms.mhchem.lk'
+        },
+        allowedHosts: ['lms.mhchem.lk']
+    },
 });
